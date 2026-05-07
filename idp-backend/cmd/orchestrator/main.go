8168760ac38eca74b5ecc0f35ee51da0347c2fe8
febri <soerjahartono22@gmail.com>
2026-05-07 16:36:39 +0700

    fix: add cors config backend

--- idp-backend/cmd/orchestrator/main.go
+++ idp-backend/cmd/orchestrator/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	// CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001", "http://192.168.2.202:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
